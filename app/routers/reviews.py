8dd40273d7e6b55429abd1547a7270c27818a353
mjkweon17 <mjkweon17@korea.ac.kr>
2023-11-11 04:37:43 +0900

    feat: get review

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -21,3 +21,19 @@ async def create_review(user_id: int, lecture_id: int, star: int, good_review: s
     db.commit()
     db.refresh(review)
     return review.review_id
+
+# 수강평 조회
+# Request: review_id
+# Response: review_id, star, good_review, bad_review, created_at, user_id, user_name
+@router.get("/{review_id}")
+async def get_review(review_id: int, db: Session = Depends(get_db)):
+    review = db.query(HKReview).filter(HKReview.review_id == review_id).first()
+    result = {}
+    result['review_id'] = review.review_id
+    result['star'] = review.star
+    result['good_review'] = review.good_review
+    result['bad_review'] = review.bad_review
+    result['created_at'] = review.created_at
+    result['user_id'] = review.user.user_id
+    result['user_name'] = review.user.user_name
+    return result

634f59b96481636540d5b14c8d2f31f9dd8608c1
mjkweon17 <mjkweon17@korea.ac.kr>
2023-11-11 04:33:51 +0900

    feat: get reviews, bookmarks

--- app/routers/users.py
+++ app/routers/users.py
@@ -35,3 +35,62 @@ async def get_user(user_id: int, db: Session = Depends(get_db)):
 
     return user_in_db
 
+# bookmark 목록 조회
+class UserBookmark(BaseModel):
+    bookmark_id: int | None = None
+    lecture_id: int | None = None
+    title: str | None = None
+    lecturer: str | None = None
+    original_price: str | None = None
+    current_price: str | None = None
+
+# Response: user_id
+# Request: bookmark 개수, UserBookmark 리스트
+@router.get("/{user_id}/bookmarks")
+async def get_bookmarks(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(HKUser).filter(HKUser.user_id == user_id).first()
+    bookmarks = user.bookmarks
+    user_bookmarks = []
+    for bookmark in bookmarks:
+        user_bookmark = UserBookmark()
+        user_bookmark.bookmark_id = bookmark.bookmark_id
+        user_bookmark.lecture_id = bookmark.lecture.lecture_id
+        user_bookmark.title = bookmark.lecture.title
+        user_bookmark.lecturer = bookmark.lecture.lecturer
+        user_bookmark.original_price = bookmark.lecture.price
+        user_bookmark.current_price = bookmark.lecture.discount_price
+        user_bookmarks.append(user_bookmark)
+    return user_bookmarks
+
+# 자신이 작성한 review 목록 조회
+class UserReview(BaseModel):
+    review_id: int | None = None
+    lecture_id: int | None = None
+    title: str | None = None
+    lecturer: str | None = None
+    review_content: str | None = None
+    review_date: datetime | None = None
+    review_star: int | None = None
+    good_review: str | None = None
+    bad_review: str | None = None
+
+# Response: user_id
+# Request: review 개수, UserReview 리스트
+@router.get("/{user_id}/reviews")
+async def get_reviews(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(HKUser).filter(HKUser.user_id == user_id).first()
+    reviews = user.reviews
+    user_reviews = []
+    for review in reviews:
+        user_review = UserReview()
+        user_review.review_id = review.review_id
+        user_review.lecture_id = review.lecture.lecture_id
+        user_review.title = review.lecture.title
+        user_review.lecturer = review.lecture.lecturer
+        user_review.review_content = review.good_review
+        user_review.review_date = review.created_at
+        user_review.review_star = review.star
+        user_review.good_review = review.good_review
+        user_review.bad_review = review.bad_review
+        user_reviews.append(user_review)
+    return user_reviews

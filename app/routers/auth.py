cc8e2835f8b26a94d92e9cf7d194a3019921b588
mjkweon17 <mjkweon17@korea.ac.kr>
2023-11-11 02:28:06 +0900

    Test

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter, Depends, Response, Request, status
-from sqlalchemy.orm import Session
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Response, Request, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 
 from database import get_db
-# from models import User
+from models import HKUser
+
+
+security = HTTPBasic()
 
 router = APIRouter()
 
 router = APIRouter(previx="/users", tags=["users"], responses={404: {"description": "Not found"}})
 
-# users 핸들러
-# @router.get("/")
+
+@app.get("/users/me")
+def read_current_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    return {"username": credentials.username, "password": credentials.password} 
